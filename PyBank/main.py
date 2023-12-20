e079a6017e3533fb692fac9534ce291bbb7d47fe
JeremyTallant <tallantj95@yahoo.com>
2023-12-19 20:14:54 -0600

    changed format of code to improve readabililty

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,7 +1,21 @@
-# Dependencies
+# Import modules
 import os
 import csv
+
+# Initialize variables
+row1 = 0
+row2 = 0
+change_month = 0
+totalmonths = 0
+net = 0
+
+# Initialize lists to store data
+date = []
+profitLosses = []
 
 # Create path to csv file
 path = os.path.join('PyBank', 'Resources', 'budget_data.csv')
 
+# Open the csv
+with open(path) as csvfile:
+    
